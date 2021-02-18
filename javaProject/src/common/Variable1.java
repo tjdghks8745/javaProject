6a006da5df8f0f9e501fe29b34cff9a5639bbbcd
admin <admin@YD01-18>
2021-02-18 12:40:43 +0900

    first fit

---
+++ javaProject/src/common/Variable1.java
@@ -0,0 +1,13 @@
+package common;
+
+public class Variable1 {
+	public static void main(String[] args) {
+		
+		int a = 150;
+		int b = 50;
+		int c = a+b;
+		
+		System.out.println("result >"+c);
+	}
+
+}
