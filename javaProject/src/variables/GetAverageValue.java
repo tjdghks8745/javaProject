a68278adc5b90ee5f336a0dc8196b3eb3c6793f8
admin <admin@YD01-18>
2021-02-19 12:32:51 +0900

    내용

--- javaProject/src/variables/GetAverageValue.java
+++ javaProject/src/variables/GetAverageValue.java
@@ -2,11 +2,11 @@ package variables;
 
 public class GetAverageValue {
 	public static void main(String[] args) {
-		int a, b;
+		int a, b; 
 		double result;
 		a = 17;
 		b = 18;
-		result = (double)(a + b) / 2; 
-		System.out.println("두수의 평균은:" + result + "입니다.");
+		result = (double)(a + b) / 2; // 이미 결과값이 실수이기 때문에 sysout에 적어도 17.0으로 결과값 표기 그래서 결과값이 아닌 함수식에 넣어야 함 .
+		System.out.println("두수의 평균은:" + result + "입니다.");  
 	}
 }
